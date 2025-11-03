8eddca4008081bfda29aa10427fde23dffaf1c0e
Aman Varma <amanverma99306@gmail.com>
2025-11-03 19:46:31 +0530

    Basic Setup Done

--- TweetApp/TweetApp/urls.py
+++ TweetApp/TweetApp/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.urls import include
+from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('tweet.urls')),
+    path('',views.home,name ='home'),
+    path('tweet/',include('tweet.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
